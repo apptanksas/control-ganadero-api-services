2a15f32c9d1a442d9c5f0719c17e24e3f996d2c7
John Ospina <jhonospina150@gmail.com>
2022-12-13 08:02:27 -0500

    add endpoint remove user invitation

---
+++ app/Http/Controllers/Api/Legacy/DeleteUserInvitationController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\Legacy;
+
+
+use App\Http\Controllers\ApiController;
+use App\Models\Legacy\UserInvitation;
+use Illuminate\Http\JsonResponse;
+
+class DeleteUserInvitationController extends ApiController
+{
+    function __invoke($id): JsonResponse
+    {
+        try {
+            /**
+             * @var $userInvitation UserInvitation
+             */
+            $userInvitation = UserInvitation::queryById($id);
+            if ($userInvitation->delete()) {
+                return $this->successResponse("OK");
+            } else {
+                return $this->internalErrorResponse("User invitation didn't could deleted, try again");
+            }
+        } catch (\Throwable $e) {
+            return $this->badRequestResponse("User invitation not found.");
+        }
+    }
+}
