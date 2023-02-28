4cb3df4bfdf51ee8748237d1d0efb365b20b5091
jospina-apptank <114356143+jospina-apptank@users.noreply.github.com>
2023-02-28 06:30:37 -0500

    add send suscriptions (#4)
    Co-authored-by: John Ospina <jhonospina150@gmail.com>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,20 +2,26 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendSubscriptionReminders;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        SendSubscriptionReminders::class
+    ];
+
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(SendSubscriptionReminders::COMMAND)->dailyAt("06:00")->runInBackground();
     }
 
     /**
@@ -25,7 +31,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- changelog.md
+++ changelog.md
@@ -1,9 +1,13 @@
 # Changelog Versions
 
+## v0.3.0
+### Added
+- Se agrega comando para enviar notificaciones push con OneSignal como recordatorio para el vencimiento de suscripciones.
+
 ## v0.2.0
-### Add
+### Added
 - Se agrega endpoint para actualizar atributos de un animal.
 
 ## v0.1.0
-### Add
+### Added
 - Se agrega endpoint para eliminar invitación de usuario.
